4c1e3e06c676103b30f8fedf3a775b66e3f9dedd
lakshya-Whitehatjr <83779709+lakshya-Whitehatjr@users.noreply.github.com>
2021-07-16 23:17:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var a = 20;
+var b = 10;
+console.log(a);
+console.log(b);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(Math.sqrt(a));
+console.log(Math.sqrt(b));
+console.log(Math.cbrt(a));
+console.log(Math.cbrt(b));
+console.log(Math.pow(a,2));
+console.log(Math.pow(b,2));
+console.log(Math.pow(a,3));
+console.log(Math.pow(b,3));
